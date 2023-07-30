44e815667ede163d4333ca4c7fd6bed8a112103d
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2023-07-31 01:01:21 +0530

    Updated

--- src/core/views/teacher.py
+++ src/core/views/teacher.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, jsonify
 import pandas as pd
-from ..models import Subject
+from ..models import Subject, semister, Result
 from .. import db
 import json
 
@@ -12,15 +12,6 @@ def home():
     return "<H1>This a Teacher Homepage</H1>"
 
 
-@TeacherViews.route("/upload", methods=['POST'])
-def upload():
-    sem = request.form.get('Sem')
-    batch = request.form.get('Batch')
-    file = request.files['file']
-
-    return render_template("/teacher/teacher_upload.html")
-
-
 @TeacherViews.route("/filter_by")
 def filter_by():
     return "<H1>This a Teacher filter_by page</H1>"
@@ -29,10 +20,12 @@ def filter_by():
 @TeacherViews.route("/addsub", methods=['POST', 'GET'])
 def addsub():
     if request.method == 'POST':
+
         sem = request.form.get('Sem')
         subCode = request.form.get('subcode')
 
         subject_entry = Subject(Sem=sem, SubCode=subCode)
+
         db.session.add(subject_entry)
         db.session.commit()
 
@@ -42,3 +35,63 @@ def addsub():
     print(type(subject_list))
 
     return render_template("teacher/teacher_add_subject.html", subject_list=subject_list)
+
+
+@TeacherViews.route('/uploads', methods=['GET', 'POST'])
+def upload_csv():
+    if request.method == 'POST':
+
+        sem = request.form.get('Sem')
+        batch = request.form.get('Batch')
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No file selected!', 'error')
+            return redirect(request.url)
+
+        if file and file.filename.endswith('.csv'):
+            try:
+                # Read the CSV data and store it in the database
+                df = pd.read_csv(file)
+
+                if sem == 1:
+                    for index, row in df.iterrows():
+                        data = Sem1(
+                            RegNo=row['RegNo'], Name=row['Name'],
+                            MAT206=row['MAT206'],
+                            CST202=row['CST202'],
+                            CST204=row['CST204'],
+                            CST206=row['CST206'],
+                            EST200=row['EST200'],
+                            MCN202=row['MCN202'],
+                            CSL202=row['CSL202'],
+                            CSL204=row['CSL204'],
+                            Earned_Credits=row['Earned_Credits'],
+                            Cumilative_Credits=row['Cumilative_Credits'],
+                            SGPA=row['SGPA'],
+                            CGPA=row['CGPA']
+                        )
+                        db.session.add(data)
+                        db.session.commit()
+
+                elif sem == 2:
+                    pass
+                elif sem == 3:
+                    pass
+
+                # Add the other semmisters here
+
+                elif sem == 8:
+                    pass
+
+                flash('CSV file uploaded successfully!', 'success')
+
+            except Exception as e:
+                flash(f'Error: {str(e)}', 'error')
+
+            return redirect(url_for('/teacher/upload'))
+
+        flash('Invalid file format. Only CSV files are allowed!', 'error')
+        return redirect(request.url)
+
+    return render_template("/teacher/teacher_upload.html")
